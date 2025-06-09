3955460a6db5712685ff298d4990f693748793d4
liamcodezz <liamn109801@gmail.com>
2025-06-08 22:49:00 -0700

    Added game window

--- src/main.c
+++ src/main.c
@@ -1,8 +1,48 @@
-#include <stdio.h>
+#include <windows.h>
+#include <stdbool.h>
 
-int main(){
+static bool quit = false;
 
-  printf("Spellcaster test");
+LRESULT CALLBACK WindowProcessMessage(HWND window_handle, UINT message, WPARAM wParam, LPARAM lParam);
 
-  return 0;
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, int nCmdShow) {
+	static WNDCLASS window_class = { 0 };
+	static const wchar_t window_class_name[] = L"Spellcaster Window Class";
+	window_class.lpszClassName = (PCSTR)window_class_name;
+	window_class.lpfnWndProc = WindowProcessMessage;
+	window_class.hInstance = hInstance;
+
+	RegisterClass(&window_class);
+
+	HWND window_handle =
+		CreateWindow((PCSTR)window_class_name, "Spellcaster", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance, NULL);
+	if(window_handle == NULL) { return -1; }
+
+	ShowWindow(window_handle, nCmdShow);
+
+	while(!quit) {
+		static MSG message = { 0 };
+		while(PeekMessage(&message, NULL, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&message);
+			DispatchMessage(&message);
+		}
+
+		// TODO: Game
+	}
+
+	return 0;
+}
+
+LRESULT CALLBACK WindowProcessMessage(HWND window_handle, UINT message, WPARAM wParam, LPARAM lParam) {
+	switch(message) {
+		case WM_QUIT:
+		case WM_DESTROY: {
+			quit = true;
+		} break;
+
+		default: {
+			return DefWindowProc(window_handle, message, wParam, lParam);
+		} break;
+	}
+	return 0;
 }
